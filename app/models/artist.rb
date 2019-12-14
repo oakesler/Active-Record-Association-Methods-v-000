1c22bceda3ad2f77b2828af02f292895a3626168
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 04:05:02 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,17 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+  
+  def get_first_song
+    self.songs.first
+  end
 
   def get_genre_of_first_song
-    self.songs.first.genre
+    self.get_first_song.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.count
   end
 
   def genre_count
