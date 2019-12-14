0b4ab0236afdbb01273ac02a499bdda5e3be3500
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 04:09:36 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,8 +11,10 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artist_array = []
-    self.artists.each do |artist|
-      artist_array << artist.name
+    #artist_array = []
+    self.artists.map do |artist|
+      #artist_array << artist.name
+      artist.name
+    end
   end
 end
